cfe2c792c296924fbfa15dbc1db657ddec16e213
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-22 18:35:30 -0300

    Melhorias finas no script BAT

--- scripts/e5r-bootstrapper.bat
+++ scripts/e5r-bootstrapper.bat
@@ -1,18 +1,12 @@
 @echo off
 
 set EXEPATH=%~dp0
-set PSSCRIPT='%EXEPATH%e5r-bootstrapper.ps1'
-set PSSCRIPTURL='https://raw.githubusercontent.com/e5r/env/master/scripts/e5r-bootstrapper.ps1'
-set PSSCRIPTDOWNLOAD="(New-Object System.Net.WebClient).DownloadFile(%PSSCRIPTURL%, %PSSCRIPT%)"
-
-echo "EXEPATH=%EXEPATH%"
-echo "PSSCRIPT=%PSSCRIPT%"
-echo "PSSCRIPTURL=%PSSCRIPTURL%"
-echo "PSSCRIPTDOWNLOAD=%PSSCRIPTDOWNLOAD%"
+set PSSCRIPT=%EXEPATH%e5r-bootstrapper.ps1
+set PSSCRIPTURL="https://raw.githubusercontent.com/e5r/env/master/scripts/e5r-bootstrapper.ps1"
+set PSSCRIPTDOWNLOAD="(New-Object System.Net.WebClient).DownloadFile('%PSSCRIPTURL%', '%PSSCRIPT%')"
 
 IF EXIST %PSSCRIPT% goto psrun
-echo "Não existe %PSSCRIPT%"
 @PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command %PSSCRIPTDOWNLOAD%
 
 :psrun
-@PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "Invoke-Expression %PSSCRIPT%"
+@PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "Invoke-Expression '%PSSCRIPT%'"
