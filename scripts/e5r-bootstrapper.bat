b823d00572d80d4092dc07ba5211efb9b9a4a10e
Erlimar Silva Campos <erlimar@gmail.com>
2014-09-22 18:29:48 -0300

    Correção em URL

--- scripts/e5r-bootstrapper.bat
+++ scripts/e5r-bootstrapper.bat
@@ -2,10 +2,16 @@
 
 set EXEPATH=%~dp0
 set PSSCRIPT='%EXEPATH%e5r-bootstrapper.ps1'
-set PSSCRIPTURL='https://raw.githubusercontent.com/e5r/env/scripts/e5r-bootstrapper.ps1'
+set PSSCRIPTURL='https://raw.githubusercontent.com/e5r/env/master/scripts/e5r-bootstrapper.ps1'
 set PSSCRIPTDOWNLOAD="(New-Object System.Net.WebClient).DownloadFile(%PSSCRIPTURL%, %PSSCRIPT%)"
 
+echo "EXEPATH=%EXEPATH%"
+echo "PSSCRIPT=%PSSCRIPT%"
+echo "PSSCRIPTURL=%PSSCRIPTURL%"
+echo "PSSCRIPTDOWNLOAD=%PSSCRIPTDOWNLOAD%"
+
 IF EXIST %PSSCRIPT% goto psrun
+echo "Não existe %PSSCRIPT%"
 @PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command %PSSCRIPTDOWNLOAD%
 
 :psrun
