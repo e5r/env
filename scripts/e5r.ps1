9c157b4ced8a7e4563c42a6e88f83afa0468a31b
Erlimar Silva Campos <erlimar@gmail.com>
2014-10-05 03:41:46 -0300

    Versão inicial de e5r.ps1 atendendo ao fluxo básico proposto

--- scripts/e5r.ps1
+++ scripts/e5r.ps1
@@ -3,10 +3,58 @@ param(
     [string[]]$args=@()
 )
 
-Write-Host "E5R Command..."
+$version = "sprint-1"
 
-$count = 0
-foreach($arg in $args) {
-    Write-Host "  arg[$count] => $arg"
-    $count++
+function Print-Usage {
+@"
+E5R Environment - Version $version
+Copyright (c) 2014 E5R Development Team
+
+Usage:
+   e5r <command> [options]
+
+"@ | Write-Host
 }
+
+Function Web-Exists([string] $url) {
+    $wr = [System.Net.WebRequest]::Create($url)
+    try {
+        $res = $wr.GetResponse()
+    } catch [System.Net.WebException] {
+        $res = $_.Exception.Response
+    }
+    $statusCode = [int]$res.StatusCode
+    if($statusCode -eq 200){
+        return $true
+    }
+    return $false
+}
+
+if($args.length -lt 1) {
+    Print-Usage
+    Exit
+}
+
+$commandPath = [IO.Path]::GetFullPath("$PSScriptRoot\..\command")
+$commandName = $args[0]
+$commandFileName = "$commandName.ps1"
+$commandFilePath = "$commandPath\$commandFileName"
+
+if ((Test-Path $commandPath) -ne 1) {
+    $outputSilent = New-Item -ItemType Directory -Force $commandPath
+}
+
+if ((Test-Path $commandFilePath) -ne 1) {
+    $url ="https://raw.githubusercontent.com/e5r/env/$version/scripts/command/$commandFileName"
+    if(Web-Exists $url) {
+        Invoke-WebRequest $url -OutFile $commandFilePath
+    }
+}
+
+if ((Test-Path $commandFilePath) -ne 1) {
+    Write-Host "Command ""$commandName"" not found!"
+    Exit
+}
+
+# TODO: Repassar argumentos do comando
+Invoke-Expression -Command 'PowerShell "$commandFilePath"'
