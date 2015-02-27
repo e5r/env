8052235008d5470768fda7351ea67efd137e9ee1
Erlimar Silva Campos <erlimar@gmail.com>
2015-02-27 18:04:45 -0300

    AspNet Environment Boot finished

--- scripts/tech/aspnet/boot.js
+++ scripts/tech/aspnet/boot.js
@@ -25,25 +25,26 @@
     sys.logTask('Booting environment ASPNET/5...');
     try {
       var _aspnetVersion = 'v1.0.0-beta3',
-          _kvmPathBase = fs.combine(sys.product.meta.userPath, '.k', 'bin'),
-          _kvmPathCmd = fs.combine(_kvmPathBase, 'kvm.cmd'),
-          _kvmPathPs = fs.combine(_kvmPathBase, 'kvm.ps1'),
+          _kvmPathBase = fs.combine(sys.product.meta.userPath, '.k'),
+          _kvmPathBin = fs.combine(_kvmPathBase, 'bin'),
+          _kvmPathCmd = fs.combine(_kvmPathBin, 'kvm.cmd'),
+          _kvmPathPs = fs.combine(_kvmPathBin, 'kvm.ps1'),
           _kvmUrlBase = 'https://raw.githubusercontent.com/aspnet/Home/{v}/kvm.{t}'.replace('{v}', _aspnetVersion),
           _kvmUrlCmd = _kvmUrlBase.replace('{t}', 'cmd'),
           _kvmUrlPs = _kvmUrlBase.replace('{t}', 'ps1');
 
-      if(!fs.directoryExists(_kvmPathBase)){
-        sys.logAction('Creating directory', _kvmPathBase + '...');
-        fs.createDirectory(_kvmPathBase);
+      if(!fs.directoryExists(_kvmPathBin)){
+        sys.logAction('Creating directory', _kvmPathBin);
+        fs.createDirectory(_kvmPathBin);
       }
 
       if(!fs.fileExists(_kvmPathCmd)){
-        sys.logAction('Downloading kvm.cmd...');
+        sys.logAction('Downloading kvm.cmd');
         _env.helpers.getWebFile('kvm.cmd', _kvmUrlCmd, _kvmPathCmd);
       }
 
       if(!fs.fileExists(_kvmPathPs)){
-        sys.logAction('Downloading kvm.ps1...');
+        sys.logAction('Downloading kvm.ps1');
         _env.helpers.getWebFile('kvm.ps1', _kvmUrlPs, _kvmPathPs);
       }
 
@@ -55,13 +56,46 @@
         throw new Error('File kvm.ps1 not found!');
       }
 
-      sys.logAction('#TODO: Atualizar PATH para todos os processos pais');
+      sys.logSubTask('Cleaning previous installations');
+      {
+        var _userPath = (su.getEnvironment('PATH', su.CONST.ENVTYPE_USER) || '').split(';'),
+            _processPath = (su.getEnvironment('PATH', su.CONST.ENVTYPE_PROCESS) || '').split(';')
+            _userNewPath = '',
+            _procesNewPath = '';
+        for(var p in _userPath){
+          var _v = _userPath[p];
+          if(_v.indexOf(_kvmPathBase) == 0) continue;
+          _userNewPath += _userNewPath.length > 0 ? ';' : '';
+          _userNewPath += _v;
+        }
+        for(var p in _processPath){
+          var _v = _processPath[p];
+          if(_v.indexOf(_kvmPathBase) == 0) continue;
+          _procesNewPath += _procesNewPath.length > 0 ? ';' : '';
+          _procesNewPath += _v;
+        }
+        su.setEnvironment('PATH', _userNewPath, su.CONST.ENVTYPE_USER);
+        su.setEnvironment('PATH', _procesNewPath, su.CONST.ENVTYPE_PROCESS);
+      }
+      sys.logSubTask('Updating environment variable PATH');
+      {
+        var _userPath = su.getEnvironment('PATH', su.CONST.ENVTYPE_USER) || '',
+            _processPath = su.getEnvironment('PATH', su.CONST.ENVTYPE_PROCESS) || '';
+        _userPath += (_userPath.length > 0 ? ';' : '') + _kvmPathBin;
+        _processPath += (_processPath.length > 0 ? ';' : '') + _kvmPathBin;
+
+        sys.logAction('Adding [' + _kvmPathBin + '] to user PATH');
+        su.setEnvironment('PATH', _userPath, su.CONST.ENVTYPE_USER);
+
+        sys.logAction('Adding [' + _kvmPathBin + '] to process PATH');
+        su.setEnvironment('PATH', _processPath, su.CONST.ENVTYPE_PROCESS);
+      }
 
     }catch(error){
-      sys.logSubTask('Could not initialize your ASPNET/5 environment.');
+      sys.logTask('Could not initialize your ASPNET/5 environment.');
       throw error;
     }
-    sys.logSubTask('Your ASPNET/5 environment is ready!');
+    sys.logTask('Your ASPNET/5 environment is ready!');
   }
 
   command.api = {
