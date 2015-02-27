e58eb986b7ad0b35a31a858782b97dc80071421d
Erlimar Silva Campos <erlimar@gmail.com>
2015-02-27 10:18:59 -0300

    AspNet boot command defined

--- scripts/tech/aspnet/boot.js
+++ scripts/tech/aspnet/boot.js
@@ -1,2 +1,32 @@
+// Copyright (c) E5R Development Team. All rights reserved.
+// Licensed under the MIT License. See LICENSE file for license information.
 
-sys.log('boot.js called!');
+(function(){ 'use strict'
+
+  var su = sys.require('sysutils.js'),
+
+      // Read comment "Runner Plugin Environment API" in <cmdrunner.js> for
+      // more information of environment API
+      _env;
+
+  /**
+   * Set environment configuration
+   */
+  function _setup(env){
+    _env = env;
+    return true;
+  }
+
+  /**
+   * Run command entry point
+   */
+  function _run(args){
+    sys.log('Running env/boot.js, args:');
+    sys.log(_env.helpers.JSON.stringify(args,null,2));
+  }
+
+  command.api = {
+    setup: _setup,
+    run: _run
+  }
+})();
