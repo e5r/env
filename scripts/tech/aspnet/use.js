01485a9685a64adf15079d986fc0dc50d739f7d7
Erlimar Silva Campos <erlimar@gmail.com>
2015-03-02 18:28:33 -0300

    Action use of env command implementend for aspnet

--- scripts/tech/aspnet/use.js
+++ scripts/tech/aspnet/use.js
@@ -8,7 +8,11 @@
 
       // Read comment "Runner Plugin Environment API" in <cmdrunner.js> for
       // more information of environment API
-      _env;
+      _env,
+
+      // ASPNET/5 information
+      _kvmPathBase = fs.combine(sys.product.meta.userPath, '.k'),
+      _kvmPathRuntimes = fs.combine(_kvmPathBase, 'runtimes');
 
   /**
    * Set environment configuration
@@ -21,10 +25,46 @@
   /**
    * Run command entry point
    */
-  function _run(args){
-    sys.logTask('Activate a runtime for ASPNET/5...');
+  function _run(version, args){
     try {
+      var _args = [
+        'kvm.ps1',
+        'install',
+        version
+      ].concat(args);
 
+      var _job = su.exec('powershell', _args, function(line){
+        if(line && line.length > 0) sys.logAction(line);
+        // Update process PATH and user PATH
+        var _matchProcess = /Adding\s(.+)\sto process PATH/g.exec(line),
+            _matchUser = /Adding\s(.+)\sto user PATH/g.exec(line),
+            _processPath = _matchProcess != null && _matchProcess.length > 1 ? _matchProcess[1] : '',
+            _userPath = _matchUser != null && _matchUser.length > 1 ? _matchUser[1] : '';
+        if(_processPath){
+          var _pPath = (su.getEnvironment('PATH', su.CONST.ENVTYPE_PROCESS) || '').split(';')
+              _pNewPath = '';
+          for(var p in _pPath){
+            var _v = _pPath[p];
+            if(_v.indexOf(_kvmPathRuntimes) == 0) continue;
+            _pNewPath += _pNewPath.length > 0 ? ';' : '';
+            _pNewPath += _v;
+          }
+          _pNewPath = _processPath + ';' + _pNewPath;
+          su.setEnvironment('PATH', _pNewPath, su.CONST.ENVTYPE_PROCESS);
+        }
+        if(_userPath){
+          var _uPath = (su.getEnvironment('PATH', su.CONST.ENVTYPE_USER) || '').split(';'),
+              _uNewPath = '';
+          for(var p in _uPath){
+            var _v = _uPath[p];
+            if(_v.indexOf(_kvmPathRuntimes) == 0) continue;
+            _uNewPath += _uNewPath.length > 0 ? ';' : '';
+            _uNewPath += _v;
+          }
+          _uNewPath = _userPath + ';' + _uNewPath;
+          su.setEnvironment('PATH', _uNewPath, su.CONST.ENVTYPE_USER);
+        }
+      });
     }catch(error){
       sys.logTask('A error ocurred on activate runtime.');
       throw error;
