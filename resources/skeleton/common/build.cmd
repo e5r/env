1c6aaae74fcecb43727edbadb8c8edec333615c8
Erlimar Silva Campos <erlimar@gmail.com>
2015-03-03 10:47:08 -0300

    Added skeleton bootstrap on build script

--- resources/skeleton/common/build.cmd
+++ resources/skeleton/common/build.cmd
@@ -1,4 +1,19 @@
 @echo off
 cd %~dp0
 
-echo Environment TODO...
+e5r help 2>nul
+
+if "%ERRORLEVEL%"=="0" goto BuildCommon_Install
+goto BuildCommon_End
+
+:BuildCommon_Install
+  echo E5R Environment Bootstrap...
+
+  set E5RREPO=https://github.com/e5r/env/raw/migrate-to-javascript
+
+  bitsadmin /TRANSFER "Downloading installer..." "%E5RREPO%/e5r-install.cmd" "%CD%\e5r-install.cmd"
+  "%CD%\e5r-install.cmd"
+
+  if exist "%CD%\e5r-install.cmd" del /F /Q "%CD%\e5r-install.cmd"
+
+:BuildCommon_End
